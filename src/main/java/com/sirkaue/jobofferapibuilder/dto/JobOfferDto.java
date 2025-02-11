1acba13a5ccc3808da7bb20ae33e743a0d32a156
sirkaue <kauekdslopes@gmail.com>
2025-02-10 22:09:32 -0300

    dto

---
+++ src/main/java/com/sirkaue/jobofferapibuilder/dto/JobOfferDto.java
@@ -0,0 +1,41 @@
+package com.sirkaue.jobofferapibuilder.dto;
+
+public class JobOfferDto {
+
+    private String title;
+    private String company;
+    private String location;
+    private double salary;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
