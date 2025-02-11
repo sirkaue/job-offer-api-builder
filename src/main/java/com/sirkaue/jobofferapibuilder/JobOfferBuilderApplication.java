4b298e5612eee50dc79a7c40714334e16353537c
sirkaue <kauekdslopes@gmail.com>
2025-02-10 22:09:04 -0300

    Initial commit

---
+++ src/main/java/com/sirkaue/jobofferapibuilder/JobOfferBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.sirkaue.jobofferapibuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOfferBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirkaue/jobofferapibuilder/JobOfferApiJobOfferBuilderImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkaue.jobofferapibuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferApiJobOfferBuilderImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
